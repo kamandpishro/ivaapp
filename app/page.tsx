879ee73b934468fe3833edcc496f4c801e844a05
kamand pishrow <kamandpishrow1379@gmail.com>
2025-11-07 18:59:26 +0330

    weekly and monthly packages structure of iva app

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/InternetPackageHeader"
 import PackagesTab from "@/components/PackagesTab"
 import Footer from "@/components/Footer"
 import DailyPackages from "@/components/DailyInternetPackages/DailyPackages" 
+
 export default function Home(){
   return(
     <div>
---
+++ components/MonthlyInternetPackages/MonthlyPackages.tsx
@@ -0,0 +1,11 @@
+export default function MonthlyPackages() {
+  return (
+    <div className="flex justify-center items-center h-[60vh] sm:h-[70vh] md:h-[80vh] lg:h-[85vh]">
+      <p className="font-bold text-black
+                   text-base sm:text-lg md:text-xl lg:text-2xl 
+                   px-4 sm:px-6 md:px-10 text-center leading-relaxed">
+                    بسته های ماهانه
+      </p>
+    </div>
+  );
+}
